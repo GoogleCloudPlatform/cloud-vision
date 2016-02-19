141758efb1d1a846da8ff83be4fb9d00b2020078
Fahim <fahimkarim@gmail.com>
2016-02-18 19:01:07 -0800

    allow upload from camera

---
+++ android/CloudVision/app/src/main/java/sample/google/com/cloudvision/PermissionUtils.java
@@ -0,0 +1,45 @@
+package sample.google.com.cloudvision;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+
+public class PermissionUtils {
+    public static boolean requestPermission(Activity activity, int requestCode, String... permissions) {
+        boolean granted = true;
+        ArrayList<String> permissionsNeeded = new ArrayList<>();
+
+        for (String s : permissions) {
+            int permissionCheck = ContextCompat.checkSelfPermission(activity, s);
+            boolean hasPermission = (permissionCheck == PackageManager.PERMISSION_GRANTED);
+            granted &= hasPermission;
+            if (!hasPermission) {
+                permissionsNeeded.add(s);
+            }
+        }
+
+        if (granted) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(activity,
+                    permissionsNeeded.toArray(new String[permissionsNeeded.size()]),
+                    requestCode);
+            return false;
+        }
+    }
+
+
+    public static boolean permissionGranted(int requestCode, int permissionCode, int[] grantResults) {
+        if (requestCode == permissionCode) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+}
