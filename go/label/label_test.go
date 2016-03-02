1d28e65a49a3ddbe97e90222e12f26a952513645
Tim Swast <swast@google.com>
2016-03-01 16:46:32 -0800

    travis/go: add config to run Go tests.
    I run only "short" tests when the credentials file is not available (for
    external pull requests). This should allow us to run most of the Go
    tests for external PRs.

--- go/label/label_test.go
+++ go/label/label_test.go
@@ -22,6 +22,12 @@ import (
 )
 
 func TestLabel(t *testing.T) {
+	// Skip tests that actually hit the API in short mode. We run with the
+	// -test.short flag in Travis from external pull requests because the
+	// credentials are not available.
+	if testing.Short() {
+		return
+	}
 	assert := assert.New(t)
 	creds := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 	const filename = "../../data/label/cat.jpg"
