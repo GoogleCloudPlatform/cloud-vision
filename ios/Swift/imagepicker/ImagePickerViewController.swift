7a49b81702a5d1be246e37746394bdacf8eda17a
Waseef Akhtar <waseefakhtar@gmail.com>
2017-07-18 17:21:50 +0500

    Resolve 'Call can throw, but it is not marked with 'try' and the error is not handled'

--- ios/Swift/imagepicker/ImagePickerViewController.swift
+++ ios/Swift/imagepicker/ImagePickerViewController.swift
@@ -64,78 +64,84 @@ extension ViewController {
             
             
             // Use SwiftyJSON to parse results
-            let json = JSON(data: dataToParse)
-            let errorObj: JSON = json["error"]
-            
-            self.spinner.stopAnimating()
-            self.imageView.isHidden = true
-            self.labelResults.isHidden = false
-            self.faceResults.isHidden = false
-            self.faceResults.text = ""
-            
-            // Check for errors
-            if (errorObj.dictionaryValue != [:]) {
-                self.labelResults.text = "Error code \(errorObj["code"]): \(errorObj["message"])"
-            } else {
-                // Parse the response
-                print(json)
-                let responses: JSON = json["responses"][0]
+            do {
+                let json = try JSON(data: dataToParse)
+                let errorObj: JSON = json["error"]
                 
-                // Get face annotations
-                let faceAnnotations: JSON = responses["faceAnnotations"]
-                if faceAnnotations != nil {
-                    let emotions: Array<String> = ["joy", "sorrow", "surprise", "anger"]
+                self.spinner.stopAnimating()
+                self.imageView.isHidden = true
+                self.labelResults.isHidden = false
+                self.faceResults.isHidden = false
+                self.faceResults.text = ""
+                
+                // Check for errors
+                if (errorObj.dictionaryValue != [:]) {
+                    self.labelResults.text = "Error code \(errorObj["code"]): \(errorObj["message"])"
+                } else {
+                    // Parse the response
+                    print(json)
+                    let responses: JSON = json["responses"][0]
                     
-                    let numPeopleDetected:Int = faceAnnotations.count
-                    
-                    self.faceResults.text = "People detected: \(numPeopleDetected)\n\nEmotions detected:\n"
-                    
-                    var emotionTotals: [String: Double] = ["sorrow": 0, "joy": 0, "surprise": 0, "anger": 0]
-                    var emotionLikelihoods: [String: Double] = ["VERY_LIKELY": 0.9, "LIKELY": 0.75, "POSSIBLE": 0.5, "UNLIKELY":0.25, "VERY_UNLIKELY": 0.0]
-                    
-                    for index in 0..<numPeopleDetected {
-                        let personData:JSON = faceAnnotations[index]
+                    // Get face annotations
+                    let faceAnnotations: JSON = responses["faceAnnotations"]
+                    if faceAnnotations != JSON.null {
+                        let emotions: Array<String> = ["joy", "sorrow", "surprise", "anger"]
                         
-                        // Sum all the detected emotions
-                        for emotion in emotions {
-                            let lookup = emotion + "Likelihood"
-                            let result:String = personData[lookup].stringValue
-                            emotionTotals[emotion]! += emotionLikelihoods[result]!
+                        let numPeopleDetected:Int = faceAnnotations.count
+                        
+                        self.faceResults.text = "People detected: \(numPeopleDetected)\n\nEmotions detected:\n"
+                        
+                        var emotionTotals: [String: Double] = ["sorrow": 0, "joy": 0, "surprise": 0, "anger": 0]
+                        var emotionLikelihoods: [String: Double] = ["VERY_LIKELY": 0.9, "LIKELY": 0.75, "POSSIBLE": 0.5, "UNLIKELY":0.25, "VERY_UNLIKELY": 0.0]
+                        
+                        for index in 0..<numPeopleDetected {
+                            let personData:JSON = faceAnnotations[index]
+                            
+                            // Sum all the detected emotions
+                            for emotion in emotions {
+                                let lookup = emotion + "Likelihood"
+                                let result:String = personData[lookup].stringValue
+                                emotionTotals[emotion]! += emotionLikelihoods[result]!
+                            }
                         }
+                        // Get emotion likelihood as a % and display in UI
+                        for (emotion, total) in emotionTotals {
+                            let likelihood:Double = total / Double(numPeopleDetected)
+                            let percent: Int = Int(round(likelihood * 100))
+                            self.faceResults.text! += "\(emotion): \(percent)%\n"
+                        }
+                    } else {
+                        self.faceResults.text = "No faces found"
                     }
-                    // Get emotion likelihood as a % and display in UI
-                    for (emotion, total) in emotionTotals {
-                        let likelihood:Double = total / Double(numPeopleDetected)
-                        let percent: Int = Int(round(likelihood * 100))
-                        self.faceResults.text! += "\(emotion): \(percent)%\n"
+                    
+                    // Get label annotations
+                    let labelAnnotations: JSON = responses["labelAnnotations"]
+                    let numLabels: Int = labelAnnotations.count
+                    var labels: Array<String> = []
+                    if numLabels > 0 {
+                        var labelResultsText:String = "Labels found: "
+                        for index in 0..<numLabels {
+                            let label = labelAnnotations[index]["description"].stringValue
+                            labels.append(label)
+                        }
+                        for label in labels {
+                            // if it's not the last item add a comma
+                            if labels[labels.count - 1] != label {
+                                labelResultsText += "\(label), "
+                            } else {
+                                labelResultsText += "\(label)"
+                            }
+                        }
+                        self.labelResults.text = labelResultsText
+                    } else {
+                        self.labelResults.text = "No labels found"
                     }
-                } else {
-                    self.faceResults.text = "No faces found"
                 }
                 
-                // Get label annotations
-                let labelAnnotations: JSON = responses["labelAnnotations"]
-                let numLabels: Int = labelAnnotations.count
-                var labels: Array<String> = []
-                if numLabels > 0 {
-                    var labelResultsText:String = "Labels found: "
-                    for index in 0..<numLabels {
-                        let label = labelAnnotations[index]["description"].stringValue
-                        labels.append(label)
-                    }
-                    for label in labels {
-                        // if it's not the last item add a comma
-                        if labels[labels.count - 1] != label {
-                            labelResultsText += "\(label), "
-                        } else {
-                            labelResultsText += "\(label)"
-                        }
-                    }
-                    self.labelResults.text = labelResultsText
-                } else {
-                    self.labelResults.text = "No labels found"
-                }
+            } catch {
+                print(error)
             }
+                
         })
         
     }
